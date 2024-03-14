92172072ae79450ef549b43245f6f1b89742d1b6
Srivastava <shivam.srivastava@hp.com>
2024-03-14 16:39:06 +0530

    file added

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "ap-south-1" # Specify the desired AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-bucket-terraform-backend" # Specify your desired bucket name (must be globally unique)
+
+  # Uncomment the following if you want to enable versioning
+  # versioning {
+  #   enabled = true
+  # }
+
+  # Uncomment the following if you want to enable server-side encryption
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }
+
+  # Uncomment the following if you want to configure logging
+  # logging {
+  #   target_bucket = "my-log-bucket" # Specify the bucket where logs will be stored
+  #   target_prefix = "logs/"
+  # }
+
+  # Uncomment the following if you want to configure access control
+  # acl    = "private" # You can set this to "private", "public-read", "public-read-write", "authenticated-read", "aws-exec-read", "bucket-owner-read", or "bucket-owner-full-control"
+
+  # Uncomment the following if you want to add tags to your bucket
+  # tags = {
+  #   Name = "MyBucket"
+  #   Environment = "Production"
+  # }
+}
+
